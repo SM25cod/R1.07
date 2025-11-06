84b66c9eda02097684844912beaa866d9b14d322
e2505557 <samuelbaur10@gmail.com>
2025-11-06 16:16:02 +0100

    06/11/2025 TP2

--- TP2/Tp2 exo 5.py
+++ TP2/Tp2 exo 5.py
@@ -1,9 +1,13 @@
 y=int(input("entrez un nombre entier:"))
-positif= y>0
-négatif= y<=0
-if y == positif:
-   print(f"le nombre entier est positif")
-elif y== négatif:
-    print(f"le nombre entier est négatif")
+
+
+if y>0:
+    print(f"le nombre entier est positif")
+
+elif y==0:
+    print(f"le nombre entier est nul")
+
+else:
+    print(f"le nombre entier est negatif")
 
         
