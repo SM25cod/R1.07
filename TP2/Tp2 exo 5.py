930b5fe464061fd15d9ed56802cedaa1cbfc6cb8
e2505557 <samuelbaur10@gmail.com>
2025-11-06 16:38:15 +0100

    06/11/2025 TP2

--- TP2/Tp2 exo 5.py
+++ TP2/Tp2 exo 5.py
@@ -1,13 +1,21 @@
 y=int(input("entrez un nombre entier:"))
 
 
-if y>0:
-    print(f"le nombre entier est positif")
 
-elif y==0:
-    print(f"le nombre entier est nul")
 
+
+
+if y==0:
+    print(f"le nombre est zéro et il est pair")
 else:
-    print(f"le nombre entier est negatif")
+    if y > 0:
+        signe = "positif"
 
-        
+    else:
+        signe = "negatif"
+
+    if y % 2 ==0:
+        parite = "pair"
+    else:
+        parite = "impair"
+    print(f"Le nombre est {signe} et {parite}")
