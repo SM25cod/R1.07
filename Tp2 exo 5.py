459f56f9e2656cdc70bdb8578fbed61b201f9b8e
SM25cod <samuelbaur10@gmail.com>
2025-11-06 15:41:14 +0100

    Add files via upload

---
+++ Tp2 exo 5.py
@@ -0,0 +1,9 @@
+y=int(input("entrez un nombre entier:"))
+positif= y>0
+négatif= y<=0
+if y == positif:
+   print(f"le nombre entier est positif")
+elif y== négatif:
+    print(f"le nombre entier est négatif")
+
+        
